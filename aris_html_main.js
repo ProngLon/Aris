4c8aa80de961b7d5125f86acab999aa4a10ea293
ProngLon <prandlon@outlook.com>
2026-05-03 17:55:12 +0800

    test

--- aris_html_main.js
+++ aris_html_main.js
@@ -1,6 +1,6 @@
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.183.2/build/three.module.js";
-import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.183.2/examples/jsm/loaders/GLTFLoader.js";
-import { AnimationMixer } from "https://cdn.jsdelivr.net/npm/three@0.183.2/build/three.module.js";
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.183.0/build/three.module.js";
+import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.183.0/examples/jsm/loaders/GLTFLoader.js";
+import { AnimationMixer } from "https://cdn.jsdelivr.net/npm/three@0.183.0/build/three.module.js";
 
 // 创建场景
 const scene = new THREE.Scene();
